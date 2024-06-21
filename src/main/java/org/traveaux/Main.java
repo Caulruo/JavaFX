16835965cd1876d2cfffae0e6dc8a038b9f33a49
Mathis Ourliac <mathis.ourliac@etu.umontpellier.fr>
2024-06-21 13:45:13 +0200

    first

---
+++ src/main/java/org/traveaux/Main.java
@@ -0,0 +1,11 @@
+package org.traveaux;
+
+public class Main {
+    public static void main(String[] args) {
+        coucou();
+    }
+
+    public void coucou(){
+        System.out.println("non");
+    }
+}
